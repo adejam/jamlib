76958a84283ebb0e664c1e996c577ba60b71ae6b
adejam <oluwaloseyifunmiadeleye1@gmail.com>
2020-07-13 21:44:22 +0100

    feat: Add fuctionality to display added book

--- js/index.js
+++ js/index.js
@@ -9,6 +9,74 @@ class Book {
   }
 }
 
+class Design {
+  static addBookToList(newBook) {
+    let status = '';
+    if (newBook.readStatus == true) {
+      status = `
+       <div class="form-check ">
+        <label class="form-check-label changeStatus" data-identity="${newBook.id}">
+          <input type="checkbox" class="form-check-input changeStatus" data-identity="${newBook.id}" checked />
+          You have read this book!
+        </label>
+       </div>
+      `;
+    } else {
+      status = `
+       <div class="form-check ">
+         <label class="form-check-label changeStatus" data-identity="${newBook.id}">
+           <input type="checkbox" class="form-check-input changeStatus" data-identity="${newBook.id}"/>
+           You should read this book!
+         </label>
+       </div>
+      `;
+    }
+    const bookRow = document.querySelector('#bookRow');
+    const itemDiv = document.createElement('article');
+    itemDiv.className = 'col-lg-3 col-md-4 col-sm-6';
+    itemDiv.innerHTML = `
+          <div class="card bbg">
+            <div class="card-header cardHeader">
+              <div class="titleDiv bg_White p_10 br_30">
+                <p class="text-success ta_center titleP">
+                  ${newBook.bookTitle}
+                </p>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="pagesDiv ">
+                <p class="text-muted ta_center">
+                  <span class="bg_White p_10 br_30">
+                    ${newBook.noOfPages} Pages
+                  </span>
+                </p>
+              </div>
+              <div>
+                <p class=" ta_center ">
+                  <a class="btn btn-sm btn-outline-danger br_30">
+                    Remove Book
+                  </a>
+                </p>
+              </div>
+              <div class="byDiv">
+                <p class="ta_center text-muted mb_10">
+                  BY
+                </p>
+                <p class="text-info ta_center bg_White p_10 br_30">
+                  ${newBook.author}
+                </p>
+              </div>
+            </div>
+            <div class="card-footer text-muted">
+              ${status}
+            </div>
+          </div>
+          `;
+
+    bookRow.appendChild(itemDiv);
+  }
+}
+
 function createBook(e) {
   e.preventDefault();
   const author = document.querySelector('#author').value;
@@ -16,4 +84,5 @@ function createBook(e) {
   const noOfPages = document.querySelector('#noOfPages').value;
   const readStatus = document.querySelector('#readStatus').checked;
   const newBook = new Book(author, bookTitle, noOfPages, readStatus);
+  Design.addBookToList(newBook);
 }
